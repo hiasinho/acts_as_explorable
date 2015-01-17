ea8bf828bda58271a8821783c0e8cbf5d8bce4de
Mathias Schneider <hiasinho@me.com>
2015-01-17 15:43:34 +0100

    Refactored parser

--- lib/acts_as_explorable/parser.rb
+++ lib/acts_as_explorable/parser.rb
@@ -1,35 +1,63 @@
 module ActsAsExplorable
   ##
-  # Returns a new TagList using the given tag string.
+  # Transforms query strings to a Hash that can be used by ActsAsExplorable.
   #
   # @example
-  #   tag_list = ActsAsTaggableOn::GenericParser.new.parse("One , Two, Three")
-  #   tag_list # ["One", "Two", "Three"]
+  #   ActsAsExplorable::Parser.transform('Zlatan in:first_name')
+  #   => { values: ["Zlatan"], params: { in: ["first_name"] } }
   class Parser
 
+    attr_reader :values, :params, :props
+
+    #
+    # Returns a transformed query Hash using the given query string
+    # @param query_string [String] A query string
+    # @param *keys [Array] An Array of transformation rules
+    #
+    # @return [Hash] Transformed query
     def self.transform(query_string, *keys)
       instance = self.new(query_string)
-      instance.parse
+      instance.parse(*keys)
     end
 
-    def initialize(query_string, *keys)
+    def initialize(query_string)
       @query_string = query_string
-      @keys = keys
+      split_query_string
     end
 
-    def parse
-      col = { props: [], values: [], params: {} }
+    #
+    # Parses the query string
+    # @param *keys [Hash] An Array of transformation rules
+    #
+    # @return [Hash] Transformed query
+    def parse(*keys)
+      split_query_string
 
-      @query_string.split(/\s+/).each { |q| q =~ /\w+:[\w,-]+/ ? col[:props] << q : col[:values] << q }
-
-      col[:props].each do |p|
+      @props.each do |p|
         key, params = p.split(':').first.to_sym, p.split(':').last.split(',')
-        next if !@keys.flatten.include?(key) && !@keys.empty?
-        col[:params][key] ||= []
-        col[:params][key] = col[:params][key] | params.map(&:downcase)
+        next if !keys.flatten.include?(key) && !keys.empty?
+        @params[key] ||= []
+        @params[key] = @params[key] | params.map(&:downcase)
       end
 
-      col.except(:props)
+      { values: @values, params: @params }
     end
+
+    private
+
+    def split_query_string
+      @values = []
+      @params = {}
+      @props  = []
+
+      @query_string.split(/\s+/).each do |q|
+        if q =~ /\w+:[\w,-]+/
+          @props << q
+        else
+          @values << q
+        end
+      end
+    end
+
   end
 end
